01928d086a87baece5f7b63f1e6c859717f8ab01
salman rajabi <77535863+dastanrab@users.noreply.github.com>
2026-02-09 11:05:01 +0330

    add

--- routes/web.php
+++ routes/web.php
@@ -96,7 +96,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/sos',function (){throw new \Exception('FUCK');});
 Route::get('pa/login', Login::class)->name('d.login');
-
+Route::middleware(['auth'])->group(function () {
+    Route::get('/docs', function () {
+        return view('scribe.index');
+    })->name('docs');
+});
 Route::group(['prefix' => 'pa', 'as' => 'd.', 'middleware' => [\App\Http\Middleware\IsAdmin::class]],function () {
 
 //    Route::post('/fcm', [FcmController::class,'store'])->name('fcm');
